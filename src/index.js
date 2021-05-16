3814b1868a62b7c55c9476da0a82db777f04a57b
Gursimran Singh <gursimransingh154@gmail.com>
2021-05-16 21:31:31 +0530

    updated the project structure.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import "./styles.scss";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let threshold = [];
+for (let tValue = 0.0; tValue <= 1.0; tValue = tValue + 0.01) {
+  threshold.push(tValue);
+}
+let options = {
+  root: null,
+  rootMargin: "0px",
+  threshold: threshold
+};
+
+let callback = (entries, observer) => {
+  entries.forEach((entry) => {
+    document.querySelector(".pencil-text").style.transform = `scale(${
+      12 * entry.intersectionRatio > 1 ? 12 * entry.intersectionRatio : 1
+    })`;
+    document.querySelector(".pencil-text").style.bottom = `${
+      140 * entry.intersectionRatio
+    }px`;
+    if (document.querySelector(".pencil-text").style.bottom === "0px") {
+      document.querySelector(".navbar").style.borderBottom =
+        "1px solid #c34e37";
+    } else {
+      document.querySelector(".navbar").style.borderBottom = "none";
+    }
+  });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
+let target = document.querySelector(".cover-img");
+observer.observe(target);
