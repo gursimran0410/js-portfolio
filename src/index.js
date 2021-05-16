3a81a8d392e98baaff7cb402e5969604eae5d5ba
Gursimran Singh <gursimransingh154@gmail.com>
2021-05-16 23:39:12 +0530

    added scroll status indicator

--- src/index.js
+++ src/index.js
@@ -1,33 +1,47 @@
 import "./styles.scss";
 
-let threshold = [];
-for (let tValue = 0.0; tValue <= 1.0; tValue = tValue + 0.01) {
-  threshold.push(tValue);
-}
-let options = {
-  root: null,
-  rootMargin: "0px",
-  threshold: threshold
+window.onscroll = () => {
+  scrollDetector();
 };
 
-let callback = (entries, observer) => {
-  entries.forEach((entry) => {
-    document.querySelector(".pencil-text").style.transform = `scale(${
-      12 * entry.intersectionRatio > 1 ? 12 * entry.intersectionRatio : 1
-    })`;
-    document.querySelector(".pencil-text").style.bottom = `${
-      140 * entry.intersectionRatio
-    }px`;
-    if (document.querySelector(".pencil-text").style.bottom === "0px") {
-      document.querySelector(".navbar").style.borderBottom =
-        "1px solid #c34e37";
-    } else {
-      document.querySelector(".navbar").style.borderBottom = "none";
-    }
-  });
+var scrollDetector = () => {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 };
 
-let observer = new IntersectionObserver(callback, options);
+// let threshold = [];
+// for (let tValue = 0.0; tValue <= 1.0; tValue = tValue + 0.01) {
+//   threshold.push(tValue);
+// }
+// let options = {
+//   root: null,
+//   rootMargin: "0px",
+//   threshold: threshold
+// };
 
-let target = document.querySelector(".cover-img");
-observer.observe(target);
+// let callback = (entries, observer) => {
+//   console.log(entries);
+// entries.forEach((entry) => {
+// document.querySelector(".pencil-text").style.transform = `scale(${
+//   12 * entry.intersectionRatio > 1 ? 12 * entry.intersectionRatio : 1
+// })`;
+// document.querySelector(".pencil-text").style.bottom = `${
+//   140 * entry.intersectionRatio
+// }px`;
+// if (document.querySelector(".pencil-text").style.bottom === "0px") {
+//   document.querySelector(".navbar").style.borderBottom =
+//     "1px solid #c34e37";
+// } else {
+//   document.querySelector(".navbar").style.borderBottom = "none";
+// }
+// });
+// };
+
+// let observer = new IntersectionObserver(callback, options);
+
+// let target = document.querySelector("body");
+// observer.observe(target);
